1120f25052949581648a5462d61ba1a2c86e91aa
Muhammad Umer Nisar <117673088+Umer-309@users.noreply.github.com>
2024-02-26 23:42:41 +0500

    Test 1 Passed

---
+++ src/App.test.tsx
@@ -0,0 +1,18 @@
+
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
+
+const MockComponent = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+    </BrowserRouter>
+  )
+}
+
+  it("should have Daraaz", () => {
+      render(<MockComponent/>)
+      const message = screen.getByLabelText("Search In Daraaz");
+      expect(message).toBeInTheDocument();
+  });
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom"
-import Layout from "./components/layout"
+import Layout from "./components/Layout"
 import LandingPage from "./pages/LandingPage"
 import ProductDetails from "./pages/ProductDetails"
 
